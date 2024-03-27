a6d76e6ab0075309ea867679a58598d4f1702b4e
Arnaud <arnaud.dumanois@etu.univ-cotedazur.fr>
2024-03-27 21:22:47 +0100

    feat: initial commit with the gaussian_blur algorithm

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Canny-Edge-Detection-with-Numba
+
+## Black and White transformation
+
+The first step  of the project, consist in transform the image in black and white. 
+
+- [ ] Check the version of the Code, if he fit with the number of channels of the image
+
+## Gaussian blur
+
+The second step of the project, consist in apply a gaussian blur to the image.
+
+- [ ] Enhanced architecture of the code
+- [ ] Fix sanitizer error, ask to the teacher
+
+## Sobel Filter
+
+The third step of the project, consist in apply the sobel filter to the image.
+
+## Thresholding
+
+The fourth step of the project, we need to find the potential edges based on thresholds
+
+
+## Hysterisis Thresholding
+
+The fifth step of the project, we need to find the edges based on the hysteresis thresholding
---
+++ gaussian_blur.py
@@ -0,0 +1,79 @@
+import numpy as np
+from PIL import Image
+from numba import cuda
+import math
+
+
+"""
+np.array([[1, 4, 6, 4, 1],
+                            [4, 16, 24, 16, 4],
+                            [6, 24, 36, 24, 6],
+                            [4, 16, 24, 16, 4],
+                            [1, 4, 6, 4, 1]])
+"""
+rgb_image = np.array(Image.open("./mona.jpeg"))
+print(rgb_image.shape)
+
+
+def generate_gaussian_kernel(width, sigma):
+    kernel = np.zeros((width, width))
+    center = width // 2
+    if width % 2 == 0:
+        raise ValueError("Width must be an odd number")
+    for i in range(width):
+        for j in range(width):
+            kernel[i, j] = np.exp(-((i - center) ** 2 + (j - center) ** 2) / (2 * sigma ** 2))
+    return kernel / kernel.sum()
+
+gaussian_kernel = generate_gaussian_kernel(25,120)
+
+# test the function
+# print(generate_gaussian_kernel(7, 1))
+
+@cuda.jit
+def apply_gaussian_kernel(input, output, kernel):
+      x, y = cuda.grid(2)
+      if y < input.shape[1] and x < input.shape[0]:
+          for c in range(input.shape[2]):
+            kernel_sum = 0
+            weighted_sum = 0
+            for a in range(kernel.shape[0]):
+              for b in range(kernel.shape[1]):
+                nx = x + a - kernel.shape[0] // 2
+                ny = y + b - kernel.shape[1] // 2
+                if nx < 0 or nx >= input.shape[1] :
+                    nx = x
+                if ny < 0 or ny >= input.shape[0] :
+                    ny = y
+                kernel_sum += kernel[a,b]
+                weighted_sum += kernel[a,b] * input[nx,ny,c]
+            output[x,y,c] = weighted_sum // kernel_sum
+
+
+def compute_thread_blocks(imagetab, block_size):
+    height, width = imagetab.shape[:2]
+    blockspergrid_x = math.ceil(width / block_size[0])
+    blockspergrid_y = math.ceil(height / block_size[1])
+    blockspergrid = (blockspergrid_y, blockspergrid_x)
+    return blockspergrid
+
+
+def call_kernel():
+    block_size = (32, 32)
+    grid_size = compute_thread_blocks(rgb_image, block_size)
+
+    print("Size de la grid",grid_size)
+    d_rgb_image = cuda.to_device(rgb_image)
+    d_blurred_image = cuda.device_array((rgb_image.shape[0], rgb_image.shape[1], rgb_image.shape[2]), dtype=np.uint8)
+    d_kernel = cuda.to_device(gaussian_kernel)
+
+    # Call the kernel
+    apply_gaussian_kernel[grid_size, block_size](d_rgb_image, d_blurred_image, d_kernel)
+    cuda.synchronize()
+
+    bw_image = d_blurred_image.copy_to_host()
+    bw_image = Image.fromarray(bw_image)
+    #bw_image.save("mona_blur.png")
+    bw_image.save("mona_blur.jpeg")
+
+call_kernel()
